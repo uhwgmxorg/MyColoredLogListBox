939bc2a58ecb0df99d887d3b068927565eebca38
Uwe on APOS-NBW10-02 <uhw@gmx.org>
2017-01-29 20:44:37 +0100

    Start.

---
+++ MyColoredLogListBox/MainWindow.xaml.cs
@@ -0,0 +1,156 @@
+﻿/******************************************************************************/
+/*                                                                            */
+/*   Program: MyColoredLogListBox                                             */
+/*   An example for a colored log ListBox                                     */
+/*                                                                            */
+/*   29.01.2017 1.0.0.0 uhwgmxorg Start                                       */
+/*                                                                            */
+/******************************************************************************/
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MyColoredLogListBox
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ObservableCollection<string> SystemColores { get; set; }
+        public string SelectedColore { get; set; }
+
+        #region INotifyPropertyChanged Properties
+        private String logMessage;
+        public String LogMessage
+        {
+            get { return logMessage; }
+            set
+            {
+                if (value != LogMessage)
+                {
+                    logMessage = value;
+                    OnPropertyChanged("LogMessage");
+                }
+            }
+        }
+        #endregion
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+
+            SystemColores = GettAllSystemColores();
+            LogMessage = "Test Log Message";
+
+            mlbl_ListBoxLog.ListBoxLogMessageAdd(LogMessage, System.Windows.Media.Brushes.Black);
+        }
+
+        /******************************/
+        /*       Button Events        */
+        /******************************/
+        #region Button Events
+
+        /// <summary>
+        /// button_Add_Click
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button_Add_Click(object sender, RoutedEventArgs e)
+        {
+            mlbl_ListBoxLog.ListBoxLogMessageAdd(LogMessage, StringToBrush(SelectedColore));
+        }
+
+        /// <summary>
+        /// button_Clear_Click
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button_Clear_Click(object sender, RoutedEventArgs e)
+        {
+            mlbl_ListBoxLog.Clear();
+        }
+
+        #endregion
+        /******************************/
+        /*      Menu Events          */
+        /******************************/
+        #region Menu Events
+
+        #endregion
+        /******************************/
+        /*      Other Events          */
+        /******************************/
+        #region Other Events
+
+        #endregion
+        /******************************/
+        /*      Other Functions       */
+        /******************************/
+        #region Other Functions
+
+        /// <summary>
+        /// GettAllSystemColores
+        /// </summary>
+        private ObservableCollection<string> GettAllSystemColores()
+        {
+            ObservableCollection<string> SColores = new ObservableCollection<string>();
+
+            // Get all SolidColorBrush in the sealed Brushes class (as strings)
+            var brushes = typeof(System.Windows.Media.Brushes).GetProperties().Where(pi => pi.PropertyType == typeof(SolidColorBrush)).Select(pi => pi.Name).ToList();
+
+            // Convert the List to a ObservableCollection
+            foreach (string cname in brushes)
+                SColores.Add(cname);
+
+            // Set the SelectedColore to Black
+            SelectedColore = SColores.FirstOrDefault(s => s.Contains("Black"));
+
+            return SColores;
+        }
+
+        /// <summary>
+        /// StringToBrush
+        /// </summary>
+        /// <param name="color"></param>
+        /// Convert a string to a SolidColorBrush e.g 
+        /// "Balck" --> public static SolidColorBrush Black { get; }
+        /// <returns></returns>
+        private SolidColorBrush StringToBrush(string color)
+        {
+            return (SolidColorBrush)new BrushConverter().ConvertFromString(color);
+        }
+
+        /// <summary>
+        /// OnPropertyChanged
+        /// </summary>
+        /// <param name="p"></param>
+        private void OnPropertyChanged(string p)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(p));
+        }
+
+        #endregion
+    }
+}
---
+++ MyColoredLogListBoxDll/MyListBoxLog.xaml.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace MyColoredLogListBoxDll
+{
+    /// <summary>
+    /// Interaktionslogik für MyListBoxLog.xaml
+    /// </summary>
+    public partial class MyListBoxLog : UserControl
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        delegate void DMessageAdd(string message, System.Windows.Media.Brush color);
+        private int _counter = 0;
+        private const int MAX_MESSGAES = 2000;
+        string Message { get; set; }
+        private System.Windows.Media.Brush MessageColor { get; set; }
+        public virtual Dispatcher DispatcherObject { get; protected set; }
+        public ObservableCollection<ListBoxLogMessageString> MessageList { get; set; }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public MyListBoxLog()
+        {
+            InitializeComponent();
+            DataContext = this;
+
+            DispatcherObject = Dispatcher.CurrentDispatcher;
+            MessageList = new ObservableCollection<ListBoxLogMessageString>();        
+        }
+
+        /******************************/
+        /*       Button Events        */
+        /******************************/
+        #region Button Events
+
+        #endregion
+        /******************************/
+        /*      Menu Events          */
+        /******************************/
+        #region Menu Events
+
+        #endregion
+        /******************************/
+        /*      Other Events          */
+        /******************************/
+        #region Other Events
+
+        #endregion
+        /******************************/
+        /*      Other Functions       */
+        /******************************/
+        #region Other Functions
+
+        /// <summary>
+        /// ListBoxLogMessageAdd
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="color"></param>
+        public void ListBoxLogMessageAdd(string message, System.Windows.Media.Brush color)
+        {
+            if (DispatcherObject.Thread != Thread.CurrentThread)
+                DispatcherObject.Invoke(new DMessageAdd(ListBoxLogMessageAdd), DispatcherPriority.ApplicationIdle, message, color);
+            else
+            {
+                MessageList.Add(new ListBoxLogMessageString(String.Format("{0}   {1:dd/MM/yyyy H:mm:ss.fff}   {2}", ++_counter, DateTime.Now, message), color, FontWeights.Normal,12));
+                listBox_LogMessages.SelectedIndex = listBox_LogMessages.Items.Count - 1;
+                listBox_LogMessages.ScrollIntoView(listBox_LogMessages.SelectedItem);
+                if (MessageList.Count > MAX_MESSGAES)
+                    MessageList.RemoveAt(0);
+            }
+        }
+
+        /// <summary>
+        /// Clear
+        /// </summary>
+        public void Clear()
+        {
+            MessageList.Clear();
+        }
+
+        /// <summary>
+        /// OnPropertyChanged
+        /// </summary>
+        /// <param name="p"></param>
+        private void OnPropertyChanged(string p)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(p));
+        }
+        #endregion
+    }
+
+    #region Help Classes
+    public class ListBoxLogMessageString
+    {
+        private int DEFAULD_FONT_SIZE = 12;
+        public string Message { get; set; }
+        public System.Windows.Media.Brush MessageColor { get; set; }
+        public FontWeight FontWeight { get; set; }
+        public int FontSize { get; set; }
+        public ListBoxLogMessageString(string message, System.Windows.Media.Brush colore)
+        {
+            Message = message;
+            MessageColor = colore;
+            FontWeight = FontWeights.Normal;
+            FontSize = DEFAULD_FONT_SIZE;
+        }
+        public ListBoxLogMessageString(string message, System.Windows.Media.Brush colore, FontWeight fontWeight)
+        {
+            Message = message;
+            MessageColor = colore;
+            FontWeight = fontWeight;
+            FontSize = DEFAULD_FONT_SIZE;
+        }
+        public ListBoxLogMessageString(string message, System.Windows.Media.Brush colore, FontWeight fontWeight,int fontSize)
+        {
+            Message = message;
+            MessageColor = colore;
+            FontWeight = fontWeight;
+            FontSize = fontSize;
+        }
+    }
+    #endregion
+}
